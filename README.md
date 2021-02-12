67834b8ed6f9929dfbafcb9ac202a6981e919ac1
Michael Riesberg-Timmer <Michael82097@gmail.com>
2021-02-11 23:10:04 -0600

    Remove redundant variable

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ NVIDIA provides a way to set the 'Digital Vibrance' which improves the contrast 
 
 If you want to run the script manually from a terminal in the background you can simply download the scripts and update the `PRIMARY_MONITOR` value to match your nvidia-settings panel then execute the script with `/bin/bash`.
 
-Before running the script you'll also want to ensure the `--query` flag matches the monitor you're going to be running on the game. To find this value use the following commands:
+If you really want to find this value using command line instead of the GUI then use the following commands:
 
 ```bash
 # Get a list of the GPUs you can choose from. Typically should only be one reasonable choice here
@@ -29,7 +29,7 @@ HDMI-0
 DP-4
 ```
 
-Then pick whichever screen looks the most reasonable. and substitute that into the main script where I have `DISPLAY` being set
+Then pick whichever screen looks the most reasonable. and substitute that into the main script where I have `PRIMARY_MONITOR` being set
 
 ## Systemd Service Installation
 
--- vibrantcs.sh
+++ vibrantcs.sh
@@ -3,27 +3,24 @@
 PRIMARY_MONITOR="DP-0"
 DEFAULT_VIBRANCE=0
 CSGO_DESIRED_VIBRANCE=800
-DISPLAY="DP-0"
 
 # Wait for our baseline to come in for comparisons
 # This is important only if the server is enabled to start on boot
 until ! [ -z "$current_vibrance" ]; do
-    current_vibrance=$(nvidia-settings -c :0 --query [gpu:0]/DigitalVibrance[${DISPLAY}] | grep "DigitalVibrance" | head -n 1 | awk '{print $NF}' | cut -d '.' -f1)
+    current_vibrance=$(nvidia-settings -c :0 --query [gpu:0]/DigitalVibrance[${PRIMARY_MONITOR}] | grep "DigitalVibrance" | head -n 1 | awk '{print $NF}' | cut -d '.' -f1)
 done
 
 while true; do
     if pgrep -x "csgo_linux64" > /dev/null; then
         if [ $current_vibrance -ne $CSGO_DESIRED_VIBRANCE ]; then
-            #notify-send "Turning On: Digital Vibrance set to '${CSGO_DESIRED_VIBRANCE:-N/A}'" --app-name="VibrantCS"
             echo "CSGO instance detected. Setting Digital Vibrance to '${CSGO_DESIRED_VIBRANCE}'"
-            nvidia-settings -c :0 -a [gpu:0]/DigitalVibrance[$PRIMARY_MONITOR]=${CSGO_DESIRED_VIBRANCE} > /dev/null
+            nvidia-settings -c :0 -a [gpu:0]/DigitalVibrance[${PRIMARY_MONITOR}]=${CSGO_DESIRED_VIBRANCE} > /dev/null
             current_vibrance=${CSGO_DESIRED_VIBRANCE}
         fi
     else
         if [ $current_vibrance -ne $DEFAULT_VIBRANCE ]; then
-            #notify-send "Turning Off: Digital Vibrance set to '${DEFAULT_VIBRANCE:-N/A}'" --app-name="VibrantCS"
             echo "CSGO no longer running. Setting Digital Vibrance to '${DEFAULT_VIBRANCE}'"
-            nvidia-settings -c :0 -a [gpu:0]/DigitalVibrance[$PRIMARY_MONITOR]=${DEFAULT_VIBRANCE} > /dev/null
+            nvidia-settings -c :0 -a [gpu:0]/DigitalVibrance[${PRIMARY_MONITOR}]=${DEFAULT_VIBRANCE} > /dev/null
             current_vibrance=${DEFAULT_VIBRANCE}
         fi
     fi
